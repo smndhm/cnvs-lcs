6b85c1c7b290be04ff879f3865135091e43cd909
smndhm <simon.duhem@gmail.com>
2019-09-28 01:29:26 +0200

    wip: example

--- docs/js/daron-crew.js
+++ docs/js/daron-crew.js
@@ -1,37 +1,26 @@
-const colors = [
-  // ["#F40080", "#D9048E", "#A6038B", "#7C038C", "#570080"],
-  // ["#FF1E42", "#CCD1D1", "#003943", "#00AC8B", "#F1B321"],
-  // ["#33A9AC", "#FFA646", "#F86041", "#982062", "#343779"],
-  // ["#1F90FF", "#1CE867", "#FBFF2C", "#E8941C", "#FF2B31"],
-  // ["#8C2786", "#26A6A6", "#F2CE1B", "#F27D16", "#F24F13"],
-  ["#FDC741", "#01B3E3", "#DA38B5", "#FF6B01", "#25CE7B"]
-];
-
-const getRandomArrayValue = array => {
-  return array[Math.floor(Math.random() * array.length)];
-};
+/**
+ * Daron Crew Example
+ * With image loading
+ */
 
 document.addEventListener("DOMContentLoaded", event => {
   let settings = {
     canvas: {
-      //canvas settings
+      // canvas settings
       width: 2100,
-      height: 2100,
-      padding: 100, //padding can be negative
-      fill: "#ffffff" //background color
+      height: 2970,
+      padding: 100 // padding can be negative
     },
     polygon: {
-      // color: getRandomArrayValue(colors),
-      // blendingMode: "multiply",
       line: {
-        color: "#eeeeee",
-        width: 5, //0 to remove border
+        color: "#000000",
+        width: 5, // 0 to remove border
         cap: "square",
         join: "round"
       },
       vertex: {
-        nb: 5000 //number of vertex
-        // onPixel: true
+        nb: 12000, // number of vertex
+        onPixel: false
       }
     },
     image: {
@@ -39,30 +28,28 @@ document.addEventListener("DOMContentLoaded", event => {
       width: 1520
     }
   };
-
   console.log(settings);
 
-  const monDessin = new LcsCnvs();
-
-  monDessin
+  // Dessin
+  const monImage = new LcsCnvs();
+  monImage
     .setCanvas(settings.canvas)
     .addImage(settings.image)
-    .then(canvas => {
-      const monImg = canvas
-        .drawTriangleAfterNewVertex(settings.polygon)
-        .canvas.toDataURL();
+    .then(image => {
+      const monImg = image // image Loaded
+        .drawTriangleAfterNewVertex(settings.polygon) // Add drawing
+        .canvas.toDataURL(); // to real image
 
       const maFeuilleBlanche = new LcsCnvs();
-
       maFeuilleBlanche
         .setCanvas({
-          width: 300,
-          height: 300,
+          width: 2100,
+          height: 2970,
           fill: "#ffffff"
         })
-        .addImage({ src: monImg, width: 300 })
+        .addImage({ src: monImg }) // add created image to blank page
         .then(canvas => {
-          canvas.append("body");
+          canvas.append("body"); // display
         });
     });
 });
