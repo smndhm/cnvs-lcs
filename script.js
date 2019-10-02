77539cf155dffa5471faa335f913f5e1c547730c
duhem-s <duhem-s@mgdis.fr>
2019-10-02 17:30:47 +0200

    wip: still trying

--- script.js
+++ script.js
@@ -17,52 +17,70 @@ JSDOM.fromFile("./docs/img/daron-crew.svg").then(dom => {
     fill: "#ffffff"
   });
 
-  for (const [i, path] of paths.entries()) {
-    //empty svg
-    svg.innerHTML = "";
-    //append path
-    svg.append(path);
-    //save
-    const pathFile = `tmp.${i}`;
-    fs.writeFile(`./${pathFile}.svg`, svg.outerHTML, function(error) {
-      if (error) throw error;
-      const monImage = new LcsCnvs();
-      monImage
-        .setCanvas({
-          width: 2100,
-          height: 2970,
-          padding: 100
-        })
-        .addImage({ src: `./${pathFile}.svg`, width: 1500 })
-        .then(image => {
-          const monGribouillis = image
-            .drawTriangleAfterNewVertex({
-              line: {
-                color: "#000000",
-                width: 5,
-                cap: "square",
-                join: "round"
-              },
-              vertex: {
-                nb: 1000,
-                onPixel: true
-              }
-            })
-            .canvas.toDataURL();
+  let promises = [];
 
-          maFeuilleBlanche.addImage({ src: monGribouillis });
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    });
-  }
-  require("fs").writeFile(
-    "draw.png",
-    maFeuilleBlanche.canvas.toDataURL().replace(/^data:image\/png;base64,/, ""),
-    "base64",
-    function(err) {
-      if (err) throw err;
+  console.log(`#1`);
+  (async function() {
+    console.log(`#2`);
+    for await (const [i, path] of paths.entries()) {
+      console.log(`#3.${i}`);
+      //empty svg
+      svg.innerHTML = "";
+      //append path
+      svg.append(path);
+      //save
+      const pathFile = `tmp.${i}`;
+      fs.writeFile(`./${pathFile}.svg`, svg.outerHTML, function(error) {
+        if (error) throw error;
+        console.log(`#4.${i}`);
+        const monImage = new LcsCnvs();
+        monImage
+          .setCanvas({
+            width: 2100,
+            height: 2970,
+            padding: 100
+          })
+          .addImage({ src: `./${pathFile}.svg`, width: 1500 })
+          .then(image => {
+            console.log(`#5.${i}`);
+            const monGribouillis = image
+              .drawTriangleAfterNewVertex({
+                line: {
+                  color: "#000000",
+                  width: 5,
+                  cap: "square",
+                  join: "round"
+                },
+                vertex: {
+                  nb: 1000,
+                  onPixel: true
+                }
+              })
+              .canvas.toDataURL();
+
+            maFeuilleBlanche.addImage({ src: monGribouillis });
+
+            console.log(`#6.${i}`);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      });
     }
-  );
+
+    console.log(`#7`);
+    fs.writeFile(
+      "draw.png",
+      maFeuilleBlanche.canvas
+        .toDataURL()
+        .replace(/^data:image\/png;base64,/, ""),
+      "base64",
+      function(err) {
+        if (err) throw err;
+        console.log(`#8`);
+      }
+    );
+    console.log(`#9`);
+  })();
+  console.log(`#10`);
 });
